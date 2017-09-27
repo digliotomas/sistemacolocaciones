2df3961d2a09611ecd06c666bc5bc8e4e79664d4
Tomas Diglio <Td.gmayocomex@gmail.com>
2017-09-27 20:23:19 -0300

    Sistema Colocaciones

--- sistemacolocaciones/views.py
+++ sistemacolocaciones/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def persona_list(request):
+    persona = Persona.objects.all
+    return render(request, 'scmt/persona_list.html', {'personas': persona})
